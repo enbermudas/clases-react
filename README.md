dbf0b7b05dc23acf1c5509361d60a2af5f5fba8c
Enrique Bermúdez <enbermudez96@gmail.com>
2023-09-04 20:47:50 -0300

    feat: actividad1

--- README.md
+++ README.md
@@ -12,3 +12,43 @@
 
 ## Reglas de Hooks
 - Solo pueden utilizarse DENTRO de un componente JSX
+
+## Actividades
+
+### Actividad 1
+
+**PARTE 1:**
+
+- Crear un componente llamado **Actividad1** dentro de /components/
+
+- Crear un nuevo componente llamado **Profile** dentro de /components/
+  - Debe recibir como props: (definir ustedes mismo los tipos)
+    - id
+    - firstName
+    - lastName
+    - age
+    - email
+    - nickname
+
+  - Renderizar todos los props (como ustedes quieran)
+
+  - El campo nickname no es obligatorio
+    - En caso de que el componente reciba "nickname", renderizar el campo en lugar de firstName y lastName
+
+  - Si age es mayor (o igual) a 18, renderizar "puedes tomar cerveza"
+
+  - Si age es menor a 18, renderizar "puedes tomar tetero"
+
+- Importar el componente **Profile** dentro **Actividad1**
+
+- Importar el componente **Actividad1** dentro de **App**
+
+**PARTE 2:**
+
+- Renderizar una lista de múltiples **Profile** dentro de **Actividad1**
+
+**PARTE 3:**
+
+- Agregar un botón de **Profile** para que, al hacer click sobre él:
+  - Haga un console log donde se muestre un mensaje con la siguiente forma: `id: email`
+  - Ejemplo: `1: kike@email.com`
--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,10 @@
-import { useState } from "react";
-
-import Navbar from "./components/Navbar";
-import Button from "./components/Button";
-
-// Mi base de datos de mentirita
-
-const usuarios = [
-  {
-    id: 1,
-    username: "enbermudas"
-  },
-  {
-    id: 2,
-    username: "miguelito"
-  },
-  {
-    id: 3,
-    username: "leizer"
-  },
-  {
-    id: 4,
-    username: "macoix"
-  }
-];
-
-// Componente
+// import Clase1 from "./components/Clase1";
 
 export default function App() {
-  // [nombreDelEstado, setNombreDelEstado]
-  // Se hace así por convención (para que otro programador entienda lo que está viendo)
-  const [username, setUsername] = useState<string>("kakarotinho");
-
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  }
-
-  // Los componentes de React (funciones) deben retonar código JSX válido (leer README)
   return (
     <>
-      <Navbar id={1} username={username} />
-
-      {/* Renderizado de listas */}
-      <ul>
-        {
-          usuarios.map((user) => {
-            return (
-              // El componente padre debe tener un campo key obligatorio
-              <li key={user.id}>{user.username}</li>
-            )
-          })
-        }
-      </ul>
-
-      {/* Reutiliza código: puedo colocar n cantidad de Button en cualquier componente */}
-      <Button id="button-1">Kike</Button>
-
-      <Button id="button-2">{"KiKiKiTo".toLowerCase()}</Button>
-
-      {/* Manejar eventos en componentes */}
-      <input type="text" onChange={handleOnChange} />
+      <div>App</div>
+      {/* <Clase1/> */}
     </>
   )
 }
